c1b1ffe1d7058e4e7b0620087ab05c9679bd2098
mulqi <mulkioi92@gmail.com>
2024-03-31 05:56:11 +0700

    feat:squib

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,13 @@ import {
 	Footer,
 	Jumbotron,
 	Navbar,
+	Squib,
 } from "@/components";
 
 export default function Home() {
 	return (
 		<>
+			<Squib />
 			<Navbar />
 			<Jumbotron />
 			<About />
---
+++ components/core/Squib/Squib.tsx
@@ -0,0 +1,86 @@
+export const Squib = () => {
+	return (
+		<>
+			<div className="pattern0 fireworks fire0">
+				<div className="ring_1"></div>
+				<div className="ring_2"></div>
+			</div>
+			<div className="pattern1 fireworks fire1">
+				<div className="ring_1"></div>
+				<div className="ring_2"></div>
+			</div>
+			<div className="pattern2 fireworks fire2">
+				<div className="ring_1"></div>
+				<div className="ring_2"></div>
+			</div>
+			<div className="pattern3 fireworks fire3">
+				<div className="ring_1"></div>
+				<div className="ring_2"></div>
+			</div>
+			<div className="pattern4 fireworks fire4">
+				<div className="ring_1"></div>
+				<div className="ring_2"></div>
+			</div>
+			<div className="pattern5 fireworks fire5">
+				<div className="ring_1"></div>
+				<div className="ring_2"></div>
+			</div>
+			<div className="pattern6 fireworks fire6">
+				<div className="ring_1"></div>
+				<div className="ring_2"></div>
+			</div>
+			<div className="pattern7 fireworks fire7">
+				<div className="ring_1"></div>
+				<div className="ring_2"></div>
+			</div>
+			<div className="pattern8 fireworks fire8">
+				<div className="ring_1"></div>
+				<div className="ring_2"></div>
+			</div>
+			<div className="pattern9 fireworks fire9">
+				<div className="ring_1"></div>
+				<div className="ring_2"></div>
+			</div>
+			<div className="pattern10 fireworks fire10">
+				<div className="ring_1"></div>
+				<div className="ring_2"></div>
+			</div>
+			<div className="pattern11 fireworks fire11">
+				<div className="ring_1"></div>
+				<div className="ring_2"></div>
+			</div>
+			<div className="pattern12 fireworks fire12">
+				<div className="ring_1"></div>
+				<div className="ring_2"></div>
+			</div>
+			<div className="pattern13 fireworks fire13">
+				<div className="ring_1"></div>
+				<div className="ring_2"></div>
+			</div>
+			<div className="pattern14 fireworks fire14">
+				<div className="ring_1"></div>
+				<div className="ring_2"></div>
+			</div>
+			<div className="pattern15 fireworks fire15">
+				<div className="ring_1"></div>
+				<div className="ring_2"></div>
+			</div>
+			<div className="pattern16 fireworks fire16">
+				<div className="ring_1"></div>
+				<div className="ring_2"></div>
+			</div>
+			<div className="pattern17 fireworks fire17">
+				<div className="ring_1"></div>
+				<div className="ring_2"></div>
+			</div>
+			<div className="pattern18 fireworks fire18">
+				<div className="ring_1"></div>
+				<div className="ring_2"></div>
+			</div>
+			<div className="pattern19 fireworks fire19">
+				<div className="ring_1"></div>
+				<div className="ring_2"></div>
+			</div>
+		</>
+	);
+};
