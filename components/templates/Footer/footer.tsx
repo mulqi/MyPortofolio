9980dcf0379790feed3ee0eef04ffea45756b607
mulqi <mulkioi92@gmail.com>
2024-03-31 21:18:04 +0700

    feat:footer

--- components/templates/Footer/footer.tsx
+++ components/templates/Footer/footer.tsx
@@ -4,10 +4,16 @@ import Link from "next/link";
 export const Footer = () => {
 	const { Profile } = useHome();
 	return (
-		<footer className="text-center mt-2">
+		<footer className="w-full flex flex-col justify-center items-center text-center mt-60 relative bg-[#3586ff] p-[20px] min-h-[100px]">
+			<div className="waves">
+				<div className="wave" id="wave1"></div>
+				<div className="wave" id="wave2"></div>
+				<div className="wave" id="wave3"></div>
+				<div className="wave" id="wave4"></div>
+			</div>
 			<ul className="flex gap-3 justify-center">
 				{Profile.map((values, index) => (
-					<div className="w-[40px] h-[40px] rounded-full hover:drop-shadow-3xl bg-[#393E46] items-center flex justify-center">
+					<div className="w-[40px] h-[40px] rounded-full hover:-translate-y-1 bg-[#393E46] items-center flex justify-center duration-300">
 						<Link href={values.link} key={index} target="_blank">
 							{values.image}
 						</Link>
@@ -15,7 +21,7 @@ export const Footer = () => {
 				))}
 			</ul>
 			<h1 className="m-9">
-				Mulqy Alkautsar &copy; {new Date().getFullYear()} All rights reserved
+				Mulqy Alkautsar &copy; {new Date().getFullYear()} | All rights reserved
 			</h1>
 		</footer>
 	);
