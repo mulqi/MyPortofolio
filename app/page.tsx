82cab12991e4e95c9d52b7217f7abf930f5a180a
mulqi <mulkioi92@gmail.com>
2024-05-16 14:11:28 +0700

    squibRm

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,11 @@ import {
 	Footer,
 	Jumbotron,
 	Navbar,
-	Squib,
 } from "@/components";
 
 export default function Home() {
 	return (
 		<>
-			<Squib />
 			<Navbar />
 			<Jumbotron />
 			<About />
